dc083cf5a496c830871bc70746ef44c5696b13c5
Raja Khan <rkhan@raileurope.com>
2015-07-14 18:53:19 -0400

    Google translation API Integration and TextPad CSS design

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -31,4 +31,54 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
+  
+  $scope.updateEditor = function() {
+	var element = document.getElementById("page_content");
+	element.style.height = element.scrollHeight + "px";
+  };
+  
+  $scope.gInit = function () {
+	  alert('1');
+	  google.load("elements", "1", {packages: "transliteration"});
+	  alert('2');
+	  google.setOnLoadCallback(function () {
+			var options = {
+					  sourceLanguage: 'en',
+					  destinationLanguage: ['hi'],
+					  shortcutKey: 'ctrl+g',
+					  transliterationEnabled: true
+					};
+					// Create an instance on TransliterationControl with the required
+					// options.
+					var control = new google.elements.transliteration.TransliterationControl(options);
+					// Enable transliteration in the textfields with the given ids.
+					var ids = ["textarea"];
+					control.makeTransliteratable(ids);
+					// Show the transliteration control which can be used to toggle between
+					// English and Hindi and also choose other destination language.
+					control.showControl('translControl');
+					alert('4');
+			   }
+	  );
+	  alert('3');
+  };
+  
+//  $scope.gLoad = function () {
+//		var options = {
+//		  sourceLanguage: 'en',
+//		  destinationLanguage: ['hi'],
+//		  shortcutKey: 'ctrl+g',
+//		  transliterationEnabled: true
+//		};
+//		// Create an instance on TransliterationControl with the required
+//		// options.
+//		var control = new google.elements.transliteration.TransliterationControl(options);
+//		// Enable transliteration in the textfields with the given ids.
+//		var ids = ["textarea"];
+//		control.makeTransliteratable(ids);
+//		// Show the transliteration control which can be used to toggle between
+//		// English and Hindi and also choose other destination language.
+//		control.showControl('translControl');
+//   };
+   
 });
