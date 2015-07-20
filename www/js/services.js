579ba4660314c363a41d9af7d385a832792c4b27
javabrown <getrk@yahoo.com>
2015-07-20 01:19:03 -0400

    Added Multi language translation support

--- www/js/services.js
+++ www/js/services.js
@@ -47,4 +47,57 @@ angular.module('starter.services', [])
       return null;
     }
   };
+})
+
+.factory('Languages', function() {
+  // Might use a resource here that returns a JSON array
+  var headerText = 'Select Language';
+  var googleToLanguageCode = 'hi';
+  
+  // Some fake testing data
+  var fonts = [{
+    id: 0,
+    name: 'HINDI',
+    code: 'hi',
+    icon: 'https://avatars1.githubusercontent.com/u/2903780?v=3&s=460'
+  }, {
+    id: 1,
+    name: 'URDU',
+    code: 'ur',
+    icon: 'https://avatars1.githubusercontent.com/u/2903780?v=3&s=460'
+  }, {
+	id: 2,
+	name: 'ARABIC',
+	code: 'ar',
+	icon: 'https://avatars1.githubusercontent.com/u/2903780?v=3&s=460'
+  }];
+
+  return {
+    all: function() {
+      return fonts;
+    },
+    remove: function(chat) {
+      fonts.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    },
+    
+    getHeaderText: function(){
+     return headerText;
+    },
+    
+    getGoogleToLanguageCode: function() {
+      return googleToLanguageCode;
+    },
+    
+    setGoogleToLanguageCode: function(toLanguage) {
+        googleToLanguageCode = toLanguage;
+    },
+  }
 });
