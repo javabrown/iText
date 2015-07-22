b9ff23a211247abec1c5f67e423133aa0ce6bb4f
javabrown <getrk@yahoo.com>
2015-07-22 00:28:57 -0400

    iText angular directive implementation.

--- www/js/directive.js
+++ www/js/directive.js
@@ -60,7 +60,43 @@ angular.module('starter.directive', [])
 		        }
 
 		    };
-		});
+		})
+		
+		.directive('itext', function() {
+		    var directive = {};
+		
+		    directive.restrict = 'E';
+		    //directive.templateUrl = "file.html";
+		    directive.template = 
+		     "<textarea ng-model='ival' name='{{name}}' rows='{{rows}}' cols='{{cols}}' />";
+		     
+		    directive.scope = {
+		 
+		    }
+		
+		    directive.link = function($scope, element, attributes) {
+		      element.css("background:url(../img/line.png) repeat;height:auto;width:100%; height: 100%;color:#333; font-weight:200; font-size:20px; font-family:'Ubuntu', Helvetica, Arial, sans-serif; border-radius:3px; line-height:2em; border:none; padding:30px; font: normal 14px verdana; line-height: 25px; padding: 2px 10px;");
+		      $scope.textarea = element;
+		      $scope.id = attributes.id;
+		      $scope.name = attributes.name;
+		      $scope.rows = attributes.rows;
+		      $scope.cols = attributes.cols;
+		      
+		      $scope.ival = localStorage.getItem('text');
+		       
+		      
+		      element.bind ("keyup", function() {
+		        localStorage.setItem('text', $scope.ival);
+		        console.log("Saved File Content:" + localStorage.getItem('text'));
+		      });
+		
+		      //element.css("background-color", "gray");
+		    }
+		
+		    return directive;
+		})
+		
+		;
 
 
         
